4e4e99225c078eb8cbc2bf7bc318c905edcb13ab
MareMare <MareMare@users.noreply.github.com>
2024-01-05 14:42:43 +0900

    Update to .NET 8.0

--- src/blazorwasm-standalone-singleOrg/blazorwasm-standalone-singleOrg/Data/GraphClientExtensions.cs
+++ src/blazorwasm-standalone-singleOrg/blazorwasm-standalone-singleOrg/Data/GraphClientExtensions.cs
@@ -1,39 +1,43 @@
-using System;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Threading;
-using System.Threading.Tasks;
+// NOTE: https://learn.microsoft.com/ja-jp/aspnet/core/blazor/security/webassembly/graph-api?view=aspnetcore-8.0&pivots=graph-sdk-5
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.Authentication.WebAssembly.Msal.Models;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Graph;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Kiota.Abstractions;
+using Microsoft.Kiota.Abstractions.Authentication;
+using IAccessTokenProvider = Microsoft.AspNetCore.Components.WebAssembly.Authentication.IAccessTokenProvider;
 
 /// <summary>
 /// Adds services and implements methods to use Microsoft Graph SDK.
 /// </summary>
 internal static class GraphClientExtensions
 {
-    /// <summary>
-    /// Extension method for adding the Microsoft Graph SDK to IServiceCollection.
-    /// </summary>
-    /// <param name="services"></param>
-    /// <param name="scopes">The MS Graph scopes to request</param>
-    /// <returns></returns>
-    public static IServiceCollection AddMicrosoftGraphClient(this IServiceCollection services, params string[] scopes)
+    public static IServiceCollection AddGraphClient(this IServiceCollection services, string? baseUrl, List<string>? scopes)
     {
-        services.Configure<RemoteAuthenticationOptions<MsalProviderOptions>>(options =>
+        if (string.IsNullOrEmpty(baseUrl) || scopes.IsNullOrEmpty())
         {
-            foreach (var scope in scopes)
+            return services;
+        }
+
+        services.Configure<RemoteAuthenticationOptions<MsalProviderOptions>>(
+            options =>
             {
-                options.ProviderOptions.AdditionalScopesToConsent.Add(scope);
-            }
-        });
+                scopes?.ForEach(scope =>
+                {
+                    options.ProviderOptions.DefaultAccessTokenScopes.Add(scope);
+                });
+            });
 
         services.AddScoped<IAuthenticationProvider, GraphAuthenticationProvider>();
-        services.AddScoped<IHttpProvider, HttpClientHttpProvider>(sp => new HttpClientHttpProvider(new HttpClient()));
-        services.AddScoped(sp => new GraphServiceClient(
-              sp.GetRequiredService<IAuthenticationProvider>(),
-              sp.GetRequiredService<IHttpProvider>()));
+
+        services.AddScoped(sp =>
+        {
+            return new GraphServiceClient(
+                new HttpClient(),
+                sp.GetRequiredService<IAuthenticationProvider>(),
+                baseUrl);
+        });
+
         return services;
     }
 
@@ -43,52 +47,31 @@ internal static class GraphClientExtensions
     /// </summary>
     private class GraphAuthenticationProvider : IAuthenticationProvider
     {
-        public GraphAuthenticationProvider(IAccessTokenProvider provider)
+        private readonly IConfiguration config;
+
+        public GraphAuthenticationProvider(IAccessTokenProvider tokenProvider, IConfiguration config)
         {
-            Provider = provider;
+            this.TokenProvider = tokenProvider;
+            this.config = config;
         }
 
-        public IAccessTokenProvider Provider { get; }
+        public IAccessTokenProvider TokenProvider { get; }
 
-        public async Task AuthenticateRequestAsync(HttpRequestMessage request)
+        public async Task AuthenticateRequestAsync(
+            RequestInformation request,
+            Dictionary<string, object>? additionalAuthenticationContext = null,
+            CancellationToken cancellationToken = default)
         {
-            var result = await Provider.RequestAccessToken(new AccessTokenRequestOptions()
-            {
-                Scopes = new[] { "https://graph.microsoft.com/User.Read" }
-            });
+            var result = await this.TokenProvider.RequestAccessToken(
+                new AccessTokenRequestOptions
+                {
+                    Scopes = this.config.GetSection("MicrosoftGraph:Scopes").Get<string[]>()
+                });
 
             if (result.TryGetToken(out var token))
             {
-                request.Headers.Authorization ??= new AuthenticationHeaderValue("Bearer", token.Value);
+                request.Headers.Add("Authorization", $"{CoreConstants.Headers.Bearer} {token.Value}");
             }
-        }
-    }
-
-    private class HttpClientHttpProvider : IHttpProvider
-    {
-        private readonly HttpClient _client;
-
-        public HttpClientHttpProvider(HttpClient client)
-        {
-            _client = client;
-        }
-
-        public ISerializer Serializer { get; } = new Serializer();
-
-        public TimeSpan OverallTimeout { get; set; } = TimeSpan.FromSeconds(300);
-
-        public void Dispose()
-        {
-        }
-
-        public Task<HttpResponseMessage> SendAsync(HttpRequestMessage request)
-        {
-            return _client.SendAsync(request);
-        }
-
-        public Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, HttpCompletionOption completionOption, CancellationToken cancellationToken)
-        {
-            return _client.SendAsync(request, completionOption, cancellationToken);
         }
     }
 }
